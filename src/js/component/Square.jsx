99027fa21c2609f91d6a99d9a8e3cc9eb1dd8ab4
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-09-28 00:45:10 -0300

    basic grid interactivity added

--- src/js/component/Square.jsx
+++ src/js/component/Square.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Square({ style }) {
+function Square({onClick}) {
   return (
-    <div className={style}></div>
+    <div className='square' onClick={onClick}></div>
   )
 }
 
